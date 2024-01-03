e0c6879a41e9ef5a76806833ec0d3678edfab7f6
JusticeandAtu <149971266+JusticeandAtu@users.noreply.github.com>
2024-01-03 08:45:50 +0200

    Update email.js

--- assets/js/email.js
+++ assets/js/email.js
@@ -31,7 +31,7 @@ function sendAppoin() {
       })
       .catch(err=>console.log(err));
 
-      let foreignPage = https://www.fresha.com/a/peakpamper-salon-spa-lusaka-9138b-kasiya-crescent-kalundu-x5xhlemt/booking?menu=true&offerItems=sv%3A15719783;
+      let foreignPage = 'https://www.fresha.com/a/peakpamper-salon-spa-lusaka-9138b-kasiya-crescent-kalundu-x5xhlemt/booking?menu=true&offerItems=sv%3A15719783';
 
       window.open(foreignPage, '_blank');
   
